8317c27a3c521bd3abc85cf83331c47c29c48741
Evgeni Genov <cmyguo@gmail.com>
2021-10-27 22:33:49 +0300

    Create 3.evenOrOdd.js

---
+++ SoftUni JS/Programming Basics with JavaScript - септември 2021/Conditional Statements - Lab/3.evenOrOdd.js
@@ -0,0 +1,13 @@
+function evenOrOdd(input) {
+    let a = Number(input[0]);
+    let b = Number(input[1]);
+
+    if (a % 2 === 0) {
+        console.log("even");
+
+    } else {
+        console.log("odd");
+    }
+
+}
+evenOrOdd(["2"])
