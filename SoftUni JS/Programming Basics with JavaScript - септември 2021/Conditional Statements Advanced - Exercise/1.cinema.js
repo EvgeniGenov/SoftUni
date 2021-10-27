6af0031ce3dd7da574ad33cab4726ec0ee0efff3
Evgeni Genov <cmyguo@gmail.com>
2021-10-27 22:34:38 +0300

    Create 1.cinema.js

---
+++ SoftUni JS/Programming Basics with JavaScript - септември 2021/Conditional Statements Advanced - Exercise/1.cinema.js
@@ -0,0 +1,22 @@
+function cinema(input) {
+    let typeOfTicket = input[0];
+    let rows = Number(input[1]);
+    let columns = Number(input[2]);
+    let income = 0;
+
+    if (typeOfTicket === "Premiere") {
+        income = rows * columns * 12;
+
+    } else if (typeOfTicket === 'Normal') {
+        income = rows * columns * 7.50;
+
+    } else {
+        income = rows * columns * 5;
+    }
+    console.log(`${income.toFixed(2)} leva`);
+
+}
+cinema(["Discount",
+    "12",
+    "30"
+])
