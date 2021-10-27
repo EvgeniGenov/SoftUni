e798f5c1291925f99a26ed5d9ca7915d737946f7
Evgeni Genov <cmyguo@gmail.com>
2021-10-27 22:55:26 +0300

    Create 04.evenPowersOf2.js

---
+++ SoftUni JS/Programming Basics with JavaScript - септември 2021/For Loop - Lab/04.evenPowersOf2.js
@@ -0,0 +1,13 @@
+function evenPowersOf2(input) {
+
+    let n = Number(input[0]);
+    let num = 1;
+
+    for (let i = 0; i <= n; i += 2) {
+
+        console.log(Math.pow(2, i));
+    }
+
+
+}
+evenPowersOf2(["6"])
