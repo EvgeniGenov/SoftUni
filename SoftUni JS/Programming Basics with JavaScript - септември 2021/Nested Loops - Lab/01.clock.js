7691116e84ab912673e5f4453f6992ed73d02e87
Evgeni Genov <cmyguo@gmail.com>
2022-05-24 13:30:29 +0300

    Update 01.clock.js

--- SoftUni JS/Programming Basics with JavaScript - септември 2021/Nested Loops - Lab/01.clock.js
+++ SoftUni JS/Programming Basics with JavaScript - септември 2021/Nested Loops - Lab/01.clock.js
@@ -1,47 +1,10 @@
-function rating(input) {
-    let index = 0;
-    let movies = Number(input[index]);
-    index++;
-    let totalRate = 0;
-    let nameMovieMax = "";
-    let nameMovieMin = "";
-    let maxRate = Number.MIN_SAFE_INTEGER;
-    let minRate = Number.MAX_SAFE_INTEGER;
+function clock() {
 
-    for (let i = 0; i < movies; i++) {
-        let name = input[index];
-        index++;
-        let rate = Number(input[index]);
-        index++;
-
-        totalRate += rate;
-
-        if (rate > maxRate) {
-            maxRate = rate;
-            nameMovieMax = name;
-        }
-        if (rate < minRate) {
-            minRate = rate;
-            nameMovieMin = name;
+    for (let h = 0; h <= 23; h++) {
+        for (let m = 0; m <= 59; m++) {
+            console.log(`${h}:${m}`);
         }
     }
 
-    let average = totalRate / movies;
-
-    console.log(`${nameMovieMax} is with highest rating: ${maxRate.toFixed(1)}`);
-    console.log(`${nameMovieMin} is with lowest rating: ${minRate.toFixed(1)}`);
-    console.log(`Average rating: ${average.toFixed(1)}`);
 }
-rating([
-    "5",
-    "A Star is Born",
-    "7.8",
-    "Creed 2",
-    "7.3",
-    "Mary Poppins",
-    "7.2",
-    "Vice",
-    "7.2",
-    "Captain Marvel",
-    "7.1"
-]);
+clock()
